2ac1a69303dc8b667d79b7505c53ef116cbeb361
Cozma Cristian <cozmacristian@HUAWEI_P9_lite_mini.localdomain>
2020-12-14 21:41:58 +0200

    remove 2 exaple persons and load them from js

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Teams job related networking (linkedin, github)
 
 - [x] create index.html file with standards
 - [x] create table that represents final design and structure (2 persons as exaple)
-- [ ] Table headers -> [firstName, lastName, links {github, linkedin} ]
-- [ ] remove 2 exaple persons and load them from js
+- [x] Table headers -> [firstName, lastName, links {github, linkedin} ]
+- [x] remove 2 exaple persons and load them from js
 - [ ] hint - TABLE -> THEAD & TBODY & TFOOT tags
 - [ ] load team.json and render them in page
---
+++ functions.js
@@ -0,0 +1,27 @@
+console.log('test script')
+
+function insertPersons(data) {
+    const tbody = document.querySelector('#list tbody');
+    tbody.innerHTML = getPersonsHtml(data);
+}
+
+function getPersonsHtml(persons) {
+    return getPersonHtml(persons[1]) + getPersonHtml(persons[0]);
+}
+
+function getPersonHtml(person) {
+    const firstName = person.firstName;
+    const lastName = person.lastName;
+    const gitHub = person.gitHub;
+    return `<tr>
+                <td>${person.firstName}</td >
+                <td>${person.lastName}</td>
+                <td><a href="https://github.com/${person.gitHub}">Github</a></td>
+            </tr > `;
+}
+
+fetch('persons.json')
+    .then(res => res.json())
+    .then(data => {
+        insertPersons(data);
+    });

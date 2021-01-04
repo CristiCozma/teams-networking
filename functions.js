a92b2d21745d030f3df8ea5d832fb87139f81b42
Cozma Cristian <cozmacristian@HUAWEI_P9_lite_mini.localdomain>
2021-01-04 20:46:13 +0200

    add team member

--- functions.js
+++ functions.js
@@ -1,3 +1,22 @@
+const API = {
+    CREATE: {
+        URL: "create.json",
+        METHOD: "GET" // POST
+    },
+    READ: {
+        URL: "persons.json",
+        METHOD: "GET"
+    },
+    UPDATE: {
+        URL: "",
+        METHOD: "GET"
+    },
+    DELETE: {
+        URL: "",
+        METHOD: "GET"
+    }
+};
+
 function insertPersons(persons) {
     const tbody = document.querySelector('#list tbody');
     tbody.innerHTML = getPersonsHtml(persons);
@@ -15,17 +34,21 @@ function getPersonHtml(person) {
                 <td>${person.firstName}</td >
                 <td>${person.lastName}</td>
                 <td><a target="_blank" href="${person.gitHub}">Github</a></td>
+                <td></td>
             </tr>`;
 }
 
 let allPersons = [];
 
-fetch('persons.json')
-    .then(res => res.json())
-    .then(data => {
-        allPersons = data;
-        insertPersons(data);
-    });
+function loadList() {
+    fetch(API.READ.URL)
+        .then(res => res.json())
+        .then(data => {
+            allPersons = data;
+            insertPersons(data);
+        });
+}
+loadList();
 
 function searchPersons(text) {
     text = text.toLowerCase();
@@ -43,4 +66,37 @@ search.addEventListener("input", e => {
     const filtrate = searchPersons(text);
 
     insertPersons(filtrate);
-})
+});
+
+function saveTeamMember() {
+    const firstName = document.querySelector("input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+    const gitHub = document.querySelector("input[name=gitHub]").value;
+
+    const person = {
+        firstName,
+        lastName,
+        gitHub
+    };
+    console.info("saving...", person, JSON.stringify(person));
+
+    fetch(API.CREATE.URL, {
+        method: API.CREATE.METHOD,
+        body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
+    })
+        .then(res => res.json())
+        .then(r => {
+            console.warn(r);
+            if (r.success) {
+                setTimeout(() => {
+                    console.info("refresh list");
+                    loadList();
+                }, 30000);
+            }
+        });
+}
+
+const saveBtn = document.querySelector("#list button");
+saveBtn.addEventListener("click", () => {
+    saveTeamMember();
+});
